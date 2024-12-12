40198d882b2f333a233347062993c7084347d015
Sabiha Anjum <drsabaanjum99@gmail.com>
2024-12-12 17:59:13 +0530

    Commit

--- smallest_positive_missing_no.py
+++ smallest_positive_missing_no.py
@@ -4,26 +4,22 @@ Note: Positive number starts from 1. The array can have negative integers too.""
 
 class Solution:
     
-    #Function to find the smallest positive number missing from the array.
-    def missingNumber(self,arr):
+    # Function to find the smallest positive number missing from the array.
+    def missingNumber(self, arr):
         n = len(arr)
+        
+        # Rearrange the array such that arr[i] = i + 1 if possible
         for i in range(n):
-            
-            #if arr[i] is within the range 1 to n
-            #and arr[i] is not placed at (arr[i]-1)th index in arr
             while 1 <= arr[i] <= n and arr[i] != arr[arr[i] - 1]:
-                #then swap arr[i] and arr[arr[i] - 1] to place arr[i]
-                #to its corresponding index
+                # Swap arr[i] and arr[arr[i] - 1] to place arr[i] at its correct position
                 temp = arr[i]
-                arr[i] = arr[arr[i] - 1]
+                arr[i] = arr[temp - 1]
                 arr[temp - 1] = temp
-                
-            #if any number is not at its corresponding index
-            #then it is the missing number
-            for i  in range(1, n+1):
-                if i != arr[i-1]:
-                    return i
-                    
-            #if all number from 1 to n are present
-            #then n + 1 is smallest missing number
-            return n+1
+
+        # Check for the first missing positive number
+        for i in range(n):
+            if arr[i] != i + 1:
+                return i + 1
+        
+        # If all numbers from 1 to n are present, the missing number is n + 1
+        return n + 1
