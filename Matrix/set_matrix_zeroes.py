5cbde2a27c1e7a686d1d63bb402fcd8395654cd0
Sabiha Anjum <drsabaanjum99@gmail.com>
2024-12-30 13:41:52 +0530

    Set Matrix zeroes

--- Matrix/set_matrix_zeroes.py
+++ Matrix/set_matrix_zeroes.py
@@ -1,31 +1,38 @@
 """You are given a 2D matrix mat[][] of size n×m. The task is to modify the matrix such that if mat[i][j] is 0, all the elements in the i-th row and j-th column are set to 0 and do it in constant space complexity."""
 
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # Result list to store the merged intervals
-        res = []
+    def setMatrixZeroes(self, mat):
+        n = len(mat)  # Number of rows
+        m = len(mat[0])  # Number of columns
 
-        # Iterate through each interval in the given list
-        for i in range(len(intervals)):
-            # Case 1: The new interval is completely before the current interval
-            if newInterval[1] < intervals[i][0]:
-                # Add the new interval to the result and append the remaining intervals
-                res.append(newInterval)
-                return res + intervals[i:]
+        c0 = 1  # Variable to track if the first column needs to be set to 0
 
-            # Case 2: The new interval is completely after the current interval
-            elif newInterval[0] > intervals[i][1]:
-                # Add the current interval to the result
-                res.append(intervals[i])
+        # Step 1: Traverse the matrix to mark rows and columns that need to be set to 0
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] == 0:
+                    # Mark the first cell of the current row
+                    mat[i][0] = 0
+                    # Mark the first cell of the current column
+                    if j == 0:
+                        c0 = 0  # First column needs to be set to 0
+                    else:
+                        mat[0][j] = 0  # Mark the first row for the current column
 
-            # Case 3: The new interval overlaps with the current interval
-            else:
-                # Merge the intervals by taking the minimum start and maximum end
-                newInterval = [
-                    min(newInterval[0], intervals[i][0]),
-                    max(newInterval[1], intervals[i][1])
-                ]
+        # Step 2: Traverse the matrix from (1, 1) to (n-1, m-1) to update cells
+        for i in range(1, n):
+            for j in range(1, m):
+                # If either the current row or column is marked, set cell to 0
+                if mat[i][0] == 0 or mat[0][j] == 0:
+                    mat[i][j] = 0
 
-        # Add the merged interval to the result after processing all intervals
-        res.append(newInterval)
-        return res
+        # Step 3: Update the first row if necessary
+        if mat[0][0] == 0:
+            for j in range(m):
+                mat[0][j] = 0
+
+        # Step 4: Update the first column if necessary
+        if c0 == 0:
+            for i in range(n):
+                mat[i][0] = 0
+
