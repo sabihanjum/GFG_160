f328d20af125d6bc31bc410b03c1662444892dca
Sabiha Anjum <drsabaanjum99@gmail.com>
2025-02-10 20:17:26 +0530

    Commit

--- Two pointer technique/count_all_triplet_with_given_sum_in_sorted_arr.py
+++ Two pointer technique/count_all_triplet_with_given_sum_in_sorted_arr.py
@@ -1,54 +1,39 @@
 """Given a sorted array arr[] and a target value, the task is to count triplets (i, j, k) of valid indices, such that arr[i] + arr[j] + arr[k] = target and i < j < k."""
 
 class Solution:
-    def countTriplets(arr, target):
-        n = len(arr)
+    def countTriplets(self, arr, target):
         res = 0
-
-        # Iterate through each element as the first element of the triplet
-        for i in range(n - 2):
+        n = len(arr)
+        
+        for i in range(n-2):
             left = i + 1
             right = n - 1
-
-            # Use two-pointer approach to find triplets
+            
             while left < right:
-
-                # Calculate the sum of the triplet
                 sum = arr[i] + arr[left] + arr[right]
-
-                # If sum is smaller, move to bigger values
                 if sum < target:
                     left += 1
-
-                # If sum is greater, move to smaller values
                 elif sum > target:
                     right -= 1
-
-                # If sum is equal to target
                 else:
-                    ele1 = arr[left]
-                    ele2 = arr[right]
-                    cnt1 = 0
-                    cnt2 = 0
-
-                    # Count frequency of the current value at 'left'
-                    while left <= right and arr[left] == ele1:
-                        left += 1
-                        cnt1 += 1
-
-                    # Count frequency of the current value at 'right'
-                    while left <= right and arr[right] == ele2:
-                        right -= 1
-                        cnt2 += 1
-
-                    # If both the elements are the same, then count of pairs 
-                    # = the number of ways to choose 2 elements among cnt1 elements
-                    if ele1 == ele2:
-                        res += (cnt1 * (cnt1 - 1)) // 2
-
-                    # If the elements are different, then count of pairs 
-                    # = product of the count of both elements
+                    if arr[left] == arr[right]:
+                        count = right - left + 1
+                        res += (count * ( count-1)) // 2
+                        break
                     else:
+                        
+                        ele1 = arr[left]
+                        ele2 = arr[right]
+                        cnt1 = 0
+                        cnt2 = 0
+                    
+                        while left <= right and arr[left] == ele1:
+                            left += 1
+                            cnt1 += 1
+                    
+                        while left <= right and arr[right] == ele2:
+                            right -= 1
+                            cnt2 += 1
                         res += (cnt1 * cnt2)
-
+                            
         return res
