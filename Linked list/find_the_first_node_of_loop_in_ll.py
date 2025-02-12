aa3778c159a716cb231e5954ef4ff827e7d314f8
Sabiha Anjum <drsabaanjum99@gmail.com>
2025-02-12 19:38:34 +0530

    Find the first node of loop in Linked List

---
+++ Linked list/find_the_first_node_of_loop_in_ll.py
@@ -0,0 +1,22 @@
+"""Given a head of the singly linked list. If a loop is present in the list then return the first node of the loop else return NULL.
+
+Custom Input format:
+A head of a singly linked list and a pos (1-based index) which denotes the position of the node to which the last node points to. If pos = 0, it means the last node points to null, indicating there is no loop."""
+
+class Solution:
+    def findFirstNode(self, head):
+        #code here
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                slow = head
+                
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        return None
